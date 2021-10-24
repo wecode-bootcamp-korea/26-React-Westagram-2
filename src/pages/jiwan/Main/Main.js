ca7036a6d1c21737a54f834b7c5b1cdabb050c46
jjeon17 <33076677+Jonathan940110@users.noreply.github.com>
2021-10-25 01:56:08 +0900

    Add : Mission 3 - 댓글 기능 구현

--- src/pages/jiwan/Main/Main.js
+++ src/pages/jiwan/Main/Main.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
 import './Main.scss';
 import './Common.scss';
-
 import Nav from '../../../components/Nav/Nav';
-
 import Feed from '../compoonents/Main/Feed/Feed';
-
 import MainRight from '../compoonents/Main/Main-Right/MainRight';
 
 class Main extends Component {
--- src/pages/jiwan/compoonents/Main/Feed/Feed.js
+++ src/pages/jiwan/compoonents/Main/Feed/Feed.js
@@ -10,7 +10,40 @@ import {
   faEllipsisH,
 } from '@fortawesome/free-solid-svg-icons';
 
-class Nav extends Component {
+class Feed extends Component {
+  constructor() {
+    super();
+    this.state = {
+      value: '',
+      commentList: [],
+    };
+  }
+
+  getValue = event => {
+    this.setState({
+      value: event.target.value,
+    });
+  };
+
+  addComment = () => {
+    this.setState({
+      commentList: this.state.commentList.concat([this.state.value]),
+      value: '',
+    });
+  };
+
+  addCommEnter = e => {
+    if (e.key === 'Enter') {
+      this.addComment();
+    }
+  };
+  // enterClick = e => {
+  //   if (e.key === 'Enter') {
+  //     this.buttonClick;
+  //     e.target.value = '';
+  //   }
+  // };
+
   render() {
     return (
       <div className="feeds">
@@ -53,18 +86,29 @@ class Nav extends Component {
               <br />
               <a href="">Check the all comments</a>
               <ul className="feed-comments">
-                <li>test</li>
+                <li>hello</li>
+                {this.state.commentList.map((comm, idx) => {
+                  return <li key={idx}>{comm}</li>;
+                })}
+                {/* map() 함수는 배열 안을 돌면서 콜백 함수를 인자로 받아 어떠한 함수처리를 해준 후 새로운 배열을 return 한다. 여기서 comm 은 입력한 값이고, 두번째 인자로는 배열의 index 가 들어온다.
+key={idx} 부분은 작성하기 않을 시 아래와 같은 경고가 뜬다. */}
               </ul>
             </div>
           </div>
           <div className="comment">
             <i className="fa fa-smile-o" />
             <input
+              // onChange={this.textChange}
+              // onKeyPress={this.enterClick}
+              onKeyPress={this.addCommEnter}
+              onChange={this.getValue}
               className="user-input"
               type="text"
               placeholder="Add Comment..."
             />
-            <button className="post">Post</button>
+            <button onClick={this.addComment} className="post">
+              Post
+            </button>
           </div>
         </div>
       </div>
@@ -72,4 +116,4 @@ class Nav extends Component {
   }
 }
 
-export default Nav;
+export default Feed;
